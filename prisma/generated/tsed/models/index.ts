57d18924f85166293c2d30c4dd6008179727e0f2
Melwin Kramer <melwin.kramer@gmail.com>
2022-11-24 14:22:04 +0100

    refactor: changed relations to a json object

--- prisma/generated/tsed/models/index.ts
+++ prisma/generated/tsed/models/index.ts
@@ -1,4 +1,2 @@
 export { AssetMetaModel } from "./AssetMetaModel";
 export { AssetModel } from "./AssetModel";
-export { ColorPaletteModel } from "./ColorPaletteModel";
-export { ResolutionModel } from "./ResolutionModel";
--- prisma/generated/tsed/repositories/index.ts
+++ prisma/generated/tsed/repositories/index.ts
@@ -1,4 +1,2 @@
 export { AssetMetasRepository } from "./AssetMetasRepository";
 export { AssetsRepository } from "./AssetsRepository";
-export { ColorPalettesRepository } from "./ColorPalettesRepository";
-export { ResolutionsRepository } from "./ResolutionsRepository";
