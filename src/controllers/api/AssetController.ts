670300316bffec213c5e6dd8e902be2a495899b3
Melwin Kramer <melwin.kramer@gmail.com>
2022-12-01 10:50:29 +0100

    feat: added authorization to upload endpoint

--- src/controllers/api/AssetController.ts
+++ src/controllers/api/AssetController.ts
@@ -7,58 +7,59 @@ import {
     PlatformMulterFile,
     PlatformResponse,
     QueryParams,
-    Res,
+    Res, UseBefore,
     ValidationError
 } from "@tsed/common";
 
 import {StoreSet} from "@tsed/core";
 import {AssetService} from "../../services/AssetService";
+import {JWTAuthorization} from "../../middleware/JWTAuthorization";
 
 @Controller("/asset")
 export class AssetController {
 
     @Inject()
-    protected assetService : AssetService;
+    protected assetService: AssetService;
 
     @Get("/")
-    //@UseBefore(JWTAuthorization)
-    @StoreSet("required-roles", ["bucket1-access"])
-    findAll(@QueryParams("page") page : number,
-            @QueryParams("pageSize") pageSize : number,
-            @QueryParams("bucket") bucket? : string) : Promise<Object> {
+    findAll(@QueryParams("page") page: number,
+            @QueryParams("pageSize") pageSize: number,
+            @QueryParams("bucket") bucket?: string): Promise<Object> {
         return this.assetService.findAll({pageSize, page, bucket});
     }
 
     @Get("/:id")
-    findById(@PathParams("id") id : string) : Object {
+    findById(@PathParams("id") id: string): Object {
         return this.assetService.findById(id);
     }
 
     @Put("/:id")
-    updateById(@PathParams("id") id : string,
-               @BodyParams() updatedAsset : Object) : Object {
+    updateById(@PathParams("id") id: string,
+               @BodyParams() updatedAsset: Object): Object {
         return this.assetService.updateById(id, updatedAsset);
     }
 
     @Delete("/:id")
-    deleteById(@PathParams("id") id : string) {
+    deleteById(@PathParams("id") id: string) {
         return this.assetService.deleteById(id);
     }
 
     @Get("/:id/b")
     async downloadById(@PathParams("id") id: string,
-                       @Res() res: PlatformResponse) : Promise<Buffer> {
+                       @Res() res: PlatformResponse): Promise<Buffer> {
         return await this.assetService.downloadAsset(id, res);
     }
 
     @Post("/:bucket")
+    @UseBefore(JWTAuthorization)
+    @StoreSet("dev-asset-bucket-rcktbs", ["bucket1-access"])
     uploadAsset(@MultipartFile("file") file: PlatformMulterFile,
                 @PathParams("bucket") bucket: string,
-                @QueryParams("systemRefId") systemRefId : string,
-                @QueryParams("context") context : string,
-                @QueryParams("k_") k_ : string
-    ) : Promise<Object> {
-        if(!file) throw new ValidationError("No file was uploaded");
+                @QueryParams("systemRefId") systemRefId: string,
+                @QueryParams("context") context: string,
+                @QueryParams("k_") k_: string
+    ): Promise<Object> {
+        if (!file) throw new ValidationError("No file was uploaded");
         return this.assetService.uploadAsset(file, bucket);
     }
 }
