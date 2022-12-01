2674240f82d8b9c7909511ddc81e63ea3e6846ee
Melwin Kramer <melwin.kramer@gmail.com>
2022-12-01 10:48:52 +0100

    feat: better JWT Authorization + error handling and dynamic role check

--- src/middleware/JWTAuthorization.ts
+++ src/middleware/JWTAuthorization.ts
@@ -16,7 +16,14 @@ export class JWTAuthorization implements MiddlewareMethods {
     jwksService: JwksService;
 
     use(@Context() $ctx: Context, @Req() $req: Req, @Next() next: NextFunction) {
-        let authToken = $req.headers['authorization'] || "";
+        let authToken : any;
+        let bucket : string = $req.path.split('/').at(-1) || "";
+
+        authToken = $req.headers['authorization'];
+
+        //Check for missing authToken..
+        if(!authToken) return next(new Unauthorized("No token found in the request header"));
+
         //Auth token starts by default with Bearer. This has to be removed.
         authToken = authToken.split(' ')[1];
 
@@ -26,20 +33,24 @@ export class JWTAuthorization implements MiddlewareMethods {
         this.jwksService.getClient().getSigningKey(kid, (error, key) => {
             if (error) return next(new Unauthorized(error.message));
             const signingKey = key?.getPublicKey();
-            JWT.verify(authToken, signingKey|| "", (err, user : ParsedJwtPayload ) => {
+            JWT.verify(authToken, signingKey|| "", (err : any, user : ParsedJwtPayload ) => {
                 if (err) return next(new Unauthorized(err.message));
                 const userRoles = user.realm_access.roles;
-                const requiredRoles = $ctx.endpoint.store.get("required-roles");
-                for (let role of requiredRoles) {
-                    if (!userRoles.includes(role)) {
-                        // @ts-ignore
-                        return next(new Unauthorized({
-                            error: "You don't have the required role(s) to access this endpoint",
-                            expected: requiredRoles,
-                            actual: userRoles
-                        }));
+                const requiredRoles = $ctx.endpoint.store.get(bucket);
+
+                //Check if bucket needs roles
+                if(requiredRoles)
+                    for (let role of requiredRoles) {
+                        if (!userRoles.includes(role)) {
+                            // @ts-ignore
+                            return next(new Unauthorized({
+                                error: "You don't have the required role(s) to access this endpoint",
+                                expected: requiredRoles,
+                                actual: userRoles
+                            }));
+                        }
                     }
-                }
+
                 return next();
             });
         });
