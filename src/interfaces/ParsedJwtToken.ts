c75e39ac6d7384118b278877ccff40dec424ba56
Melwin Kramer <melwin.kramer@gmail.com>
2023-01-09 11:52:42 +0100

    refactor: reused already defined type

--- src/interfaces/ParsedJwtToken.ts
+++ src/interfaces/ParsedJwtToken.ts
@@ -1,39 +1,11 @@
+import {ParsedJwtPayload} from "./ParsedJwtPayload";
+
 export interface ParsedJwtToken {
     "header": {
         alg: string,
         typ: string,
         kid: string
     },
-    "payload": {
-        "exp": number,
-        "iat": number,
-        "jti": string,
-        "iss": string,
-        "aud": [string],
-        "sub": string,
-        "typ": string,
-        "azp": string,
-        "session_state": string,
-        "acr": string,
-        "realm_access": {
-            "roles": [string]
-        },
-        "resource_access": {
-            "realm-management": {
-                "roles": [string]
-            },
-            "account": {
-                "roles": [string]
-            }
-        },
-        "scope": string,
-        "sid": string,
-        "email_verified": boolean,
-        "name": string,
-        "preferred_username": string,
-        "given_name": string,
-        "family_name": string,
-        "email": string
-    },
+    "payload": ParsedJwtPayload,
     "signature": string
 }
