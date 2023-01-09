ef55f72422c2386368a4291cceda97f5a4792370
Melwin Kramer <melwin.kramer@gmail.com>
2023-01-09 11:55:55 +0100

    docs: added swagger documentations + feat: resize endpoint

--- src/controllers/api/AssetController.ts
+++ src/controllers/api/AssetController.ts
@@ -1,5 +1,5 @@
 import {Controller, Inject} from "@tsed/di";
-import {Delete, Get, Post, Put, Summary} from "@tsed/schema";
+import {Delete, Get, Post, Put, Returns, Summary} from "@tsed/schema";
 import {
     BodyParams, MulterOptions,
     MultipartFile,
@@ -7,13 +7,16 @@ import {
     PlatformMulterFile,
     PlatformResponse,
     QueryParams,
-    Res
+    Res, UseBefore
 } from "@tsed/common";
 
-import {StoreSet} from "@tsed/core";
+import {StoreSet, Type} from "@tsed/core";
 import {AssetService} from "../../services/AssetService";
 import {FormattedAsset} from "../../interfaces/FormattedAsset";
 import {AssetFindService} from "../../services/AssetFindService";
+import {BadRequest, Exception, Unauthorized} from "@tsed/exceptions";
+import {JWTAuthorization} from "../../middleware/JWTAuthorization";
+import {ImageProxyService} from "../../services/ImageProxyService";
 
 @Controller("/asset")
 export class AssetController {
@@ -22,6 +25,14 @@ export class AssetController {
     protected assetService: AssetService;
     @Inject()
     protected assetFindService: AssetFindService;
+    @Inject()
+    protected imageProxyService : ImageProxyService;
+
+    @Get("/resize/:id")
+    async resizeAsset(@PathParams("id") id: string) : Promise<any> {
+        let asset = await this.assetFindService.findById(id)
+        return this.imageProxyService.resizeAsset(asset);
+    }
 
     /**Every GET Request <br>
      * GET every Asset as pageable <br>
@@ -30,6 +41,7 @@ export class AssetController {
     * */
     @Get("/")
     @Summary("Get all assets as a pageable. optional: in which bucket")
+    @Returns(200, FormattedAsset).Description("Returns an formatted version of an array of assets")
     findAll(@QueryParams("page") page: number,
             @QueryParams("pageSize") pageSize: number,
             @QueryParams("bucket") bucket?: string) : Promise<FormattedAsset[]> {
@@ -44,6 +56,7 @@ export class AssetController {
 
     @Get("/:id/b")
     @Summary("Download a file given by an ID")
+    @Returns(200, Buffer).Description("Returns an formatted version of an array of assets")
     async downloadById(@PathParams("id") id: string,
                        @Res() res: PlatformResponse): Promise<Buffer> {
         return await this.assetFindService.downloadAsset(id, res);
@@ -65,11 +78,14 @@ export class AssetController {
     }
 
     @Post("/")
-    @Summary("Upload file to bucket and save meta data to the database")
-    //@UseBefore(JWTAuthorization)
+    @UseBefore(JWTAuthorization)
     @StoreSet("dev-asset-bucket-rcktbs", ["bucket1-access"])
+    @Summary("Upload file to bucket and save meta data to the database")
+    @Returns(200, FormattedAsset).Description("Formatted Asset")
+    @Returns(401, Unauthorized).Description("If JWT Token isn't valid for the bucket")
+    @Returns(400, Exception).Description("On Error")
     async uploadAsset(@MultipartFile("file") files: PlatformMulterFile[],
-                @QueryParams("bucket") bucket: string
+                      @QueryParams("bucket") bucket: string
     ) : Promise<FormattedAsset | FormattedAsset[]> {
         let assets : FormattedAsset[] = [];
         for(let file of files) assets.push(await this.assetService.uploadAsset(file, bucket));
@@ -77,21 +93,29 @@ export class AssetController {
     }
 
     @Post("/batch/:bucket")
+    @Summary("Upload multiple downloaded urls to bucket and save meta data")
+    @Returns(200, FormattedAsset).Description("Returns an formatted version of an array of assets")
     async uploadDownloadedAssets(@BodyParams("urls") urls: string[],
                                  @PathParams("bucket") bucket: string
-    ) : Promise<any> {
+    ) : Promise<FormattedAsset[]> {
         return await this.assetService.batchUpload(urls, bucket);
     }
 
     @Post("/analyze-file")
+    @Summary("No saving or upload. Just analyzing file")
+    @Returns(200, FormattedAsset).Description("Return a formatted version of the asset")
     analyzeAssetByFile(@MultipartFile("file") file : PlatformMulterFile) : Promise<FormattedAsset> {
         return this.assetService.analyzeFile(file);
     }
     @Post("/analyze-url")
+    @Summary("No saving or upload. Just analyzing downloaded url")
+    @Returns(200, FormattedAsset).Description("Return a formatted version of the asset")
     analyzeAssetByUrl(@BodyParams() body : {url: string}) : Promise<FormattedAsset> {
         return this.assetService.analyzeUrl(body.url);
     }
     @Post("/analyze-url/save")
+    @Summary("Save a file to the database + adding analyzed time")
+    @Returns(200, FormattedAsset).Description("Return a formatted version of the asset")
     saveAnalyzedUrl(@BodyParams() body : {url: string}) : Promise<FormattedAsset> {
         return this.assetService.saveAnalyzedUrl(body.url);
     }
